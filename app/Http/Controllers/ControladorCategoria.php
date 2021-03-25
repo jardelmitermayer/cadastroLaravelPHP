cd0296c0f17d22d5c46ec1ea4c4015f5e23b0817
mitermayer18 <jardelska@gmail.com>
2021-03-25 02:32:46 -0300

    feat:adicionando categorias ao select categoria na view produtos por meio do JavaScript

--- app/Http/Controllers/ControladorCategoria.php
+++ app/Http/Controllers/ControladorCategoria.php
@@ -100,4 +100,9 @@ class ControladorCategoria extends Controller
         }
         return redirect('/categorias');
     }
+    public function indexJson()
+    {
+        $cats = Categoria::all();
+        return json_encode($cats);
+    }
 }
--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -55,9 +55,9 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="nomeProduto" class="control-label">Nome do Produto</label>
+                        <label for="categoriaProduto" class="control-label">Categoria</label>
                         <div class="input-group">
-                            <select class="form-control" id="departamentoProduto">
+                            <select class="form-control" id="categoriaProduto">
                                 
                             </select>
                         </div>
@@ -84,5 +84,19 @@
             $('#quantidadeProduto').val('');
             $('#dlgProdutos').modal('show');        
         }
+        function carregarCategorias (){
+            $.getJSON('/api/categorias', function(data) {
+                console.log(data);
+                
+                for(i=0;i<data.length;i++){
+                    opcao = '<option value="' + data[i].id + '">' + data[i].nome + '</option>';
+                    $('#categoriaProduto').append(opcao);
+                }
+            })
+        }
+
+        $(function(){
+            carregarCategorias();
+        })
     </script>
 @endsection
