3d415387f2b7b1ac9b2ae4034305802635b0632f
mitermayer18 <jardelska@gmail.com>
2021-03-25 02:06:09 -0300

    feat:criando modal na view de produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -27,7 +27,7 @@
         </div>
     </div>
 
-<div class="model" tabindex="-1" role="dialog" id="dlgProdutos">
+<div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <form action="" class="form-horizontal" id="formProduto">
@@ -35,7 +35,37 @@
                     <h5 class="modal-title">Novo Produto</h5>
                 </div>
                 <div class="modal-body">
-                    
+                    <input type="hidden" id="id" class="form-control">
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label">Nome do Produto</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="nomeProduto" placeholder="Nome do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="precoProduto" class="control-label">Preço</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="precoProduto" placeholder="Preço do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="quantidadeProduto" class="control-label">Quantidade</label>
+                        <div class="input-group">
+                            <input type="number" class="form-control" id="nomeProduto" placeholder="Quantidade do Produto">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label">Nome do Produto</label>
+                        <div class="input-group">
+                            <select class="form-control" id="departamentoProduto">
+                                
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <button type="submit" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                 </div>
             </form>
         </div>
